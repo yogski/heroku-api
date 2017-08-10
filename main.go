866e2c6c2d64c85f81d9b0b967255921945a8912
Yogski <yogisaputro@outlook.com>
2017-08-10 13:19:41 +0700

    C:/Program Files/Git/db

--- main.go
+++ main.go
@@ -2,18 +2,22 @@ package main
 
 import (
 	"bytes"
+    "fmt"
     "log"
     "net/http"
     "os"
     "strconv"
-    
-	"github.com/gin-gonic/gin"
-	"github.com/russross/blackfriday"
-	_ "github.com/heroku/x/hmetrics/onload"
+    "time"
+    "database/sql"
+
+    "github.com/gin-gonic/gin"
+    "github.com/russross/blackfriday"
+    _ "github.com/lib/pq"
 )
 
 var (
 	repeat int
+	db *sql.DB
 )
 
 const SERVER_PORT string = "8080"
@@ -25,6 +29,39 @@ func repeatHandler(c *gin.Context) {
     }
     c.String(http.StatusOK, buffer.String())
 }
+
+func dbFunc(c *gin.Context) {
+    if _, err := db.Exec("CREATE TABLE IF NOT EXISTS ticks (tick timestamp)"); err != nil {
+        c.String(http.StatusInternalServerError,
+            fmt.Sprintf("Error creating database table: %q", err))
+        return
+    }
+
+    if _, err := db.Exec("INSERT INTO ticks VALUES (now())"); err != nil {
+        c.String(http.StatusInternalServerError,
+            fmt.Sprintf("Error incrementing tick: %q", err))
+        return
+    }
+
+    rows, err := db.Query("SELECT tick FROM ticks")
+    if err != nil {
+        c.String(http.StatusInternalServerError,
+            fmt.Sprintf("Error reading ticks: %q", err))
+        return
+    }
+
+    defer rows.Close()
+    for rows.Next() {
+        var tick time.Time
+        if err := rows.Scan(&tick); err != nil {
+          c.String(http.StatusInternalServerError,
+            fmt.Sprintf("Error scanning ticks: %q", err))
+            return
+        }
+        c.String(http.StatusOK, fmt.Sprintf("Read from DB: %s\n", tick.String()))
+    }
+}
+
 
 func main() {
 	var err error
@@ -41,6 +78,11 @@ func main() {
         repeat = 5
     }
 
+    db, err = sql.Open("postgres", os.Getenv("DATABASE_URL"))
+    if err != nil {
+        log.Fatalf("Error opening database: %q", err)
+    }
+
 	router := gin.New()
 	router.Use(gin.Logger())
 	router.LoadHTMLGlob("templates/*.tmpl.html")
@@ -55,6 +97,7 @@ func main() {
 	})
 	
 	router.GET("/repeat", repeatHandler)
+	router.GET("/db", dbFunc)
 
 	router.Run(":" + port)
 }
