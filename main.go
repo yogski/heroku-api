3ab50f1a7a0f89db71860ac6aa2b8900e0f8a9dc
Yogski <yogisaputro@outlook.com>
2017-08-10 12:57:03 +0700

    Markdown demo dependency

--- main.go
+++ main.go
@@ -1,19 +1,21 @@
 package main
 
 import (
-	"log"
+	_ "log"
 	"net/http"
 	"os"
 
 	"github.com/gin-gonic/gin"
+	"github.com/russross/blackfriday"
 	_ "github.com/heroku/x/hmetrics/onload"
 )
 
+const SERVER_PORT string = "8080"
+
 func main() {
 	port := os.Getenv("PORT")
-
 	if port == "" {
-		log.Fatal("$PORT must be set")
+		port = SERVER_PORT
 	}
 
 	router := gin.New()
@@ -25,5 +27,9 @@ func main() {
 		c.HTML(http.StatusOK, "index.tmpl.html", nil)
 	})
 
+	router.GET("/mark", func(c *gin.Context) {
+  		c.String(http.StatusOK, string(blackfriday.MarkdownBasic([]byte("**hi!**"))))
+	})
+	
 	router.Run(":" + port)
 }
